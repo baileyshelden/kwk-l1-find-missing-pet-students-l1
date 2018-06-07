fd2069987d3f5c4f47a3210aabb9cc99316c3ef1
baileyshelden <bailey7748@gmail.com>
2018-06-07 22:03:27 +0000

    automatically backed up by learn

--- cars.rb
+++ cars.rb
@@ -36,17 +36,6 @@ end
 car_1 = car.new("","","")
 car_2 = car.new("","","")
 car_3 = car.new("","","")
-car_4 = car.new("","","")
-car_5 = car.new("","","")
-car_6 = car.new("","","")
-car_7 = car.new("","","")
-car_8 = car.new("","","")
-car_9 = car.new("","","")
-car_10= car.new("","","")
-car_1 = car.new("","","")
-car_2 = car.new("","","")
-car_3 = car.new("","","")
-car_12 = car.new("","","")
-car_13 = car.new("","","")
+
 
 
